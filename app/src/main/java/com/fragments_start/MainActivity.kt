b11d2f97718894fa29c5d34e1e8ac47d2e1b9232
Sergey Smirnov <cepera.cmuphob@gmail.com>
2023-12-04 21:36:36 +0200

    toolbar fixed

--- app/src/main/java/com/fragments_start/MainActivity.kt
+++ app/src/main/java/com/fragments_start/MainActivity.kt
@@ -6,7 +6,11 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 
 import com.fragments_start.databinding.ActivityMainBinding
@@ -15,6 +19,8 @@ import com.fragments_start.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,48 +32,51 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true);
 
-//        getSupportActionBar()?.setTitle("fdfdf")
+        navHostFragment =
+            supportFragmentManager.findFragmentById(binding.mainList.id) as NavHostFragment;
+        supportActionBar?.setTitle(R.string.put_text_here)
+
+        navController = navHostFragment.findNavController()
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        menuInflater.inflate(R.menu.menu_action, menu)
+//        // return true
+//        return super.onCreateOptionsMenu(menu)
+//    }
+
+//    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
 //
-//        supportActionBar?.setTitle(R.string.put_text_here)
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-
-        //     NavigationUI.setupWithNavController()
-//        supportActionBar!!.setTitle(R.string.put_text_here)
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_action, menu)
-        // return true
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-
-        R.id.action_favorite -> {
-            // User chooses the "Favorite" action. Mark the current item as a
-            // favorite.
-            Toast.makeText(applicationContext, "action_favorite", Toast.LENGTH_SHORT).show()
-
-            findNavController(binding.mainList.id).popBackStack()
-            true
-        }
-
-        R.id.action_settings -> {
-            // User chooses the "Settings" item. Show the app settings UI.
-            Toast.makeText(applicationContext, "action_settings", Toast.LENGTH_SHORT).show()
-            true
-        }
-
-
-        else -> {
-            // The user's action isn't recognized.
-            // Invoke the superclass to handle it.
-            super.onOptionsItemSelected(item)
-        }
-    }
+//        R.id.action_favorite -> {
+//            // User chooses the "Favorite" action. Mark the current item as a
+//            // favorite.
+//            Toast.makeText(applicationContext, "action_favorite", Toast.LENGTH_SHORT).show()
+//
+//            findNavController(binding.mainList.id).popBackStack()
+//            true
+//        }
+//
+//        R.id.action_settings -> {
+//            // User chooses the "Settings" item. Show the app settings UI.
+//            Toast.makeText(applicationContext, "action_settings", Toast.LENGTH_SHORT).show()
+//            true
+//        }
+//
+//
+//        else -> {
+//            // The user's action isn't recognized.
+//            // Invoke the superclass to handle it.
+//            super.onOptionsItemSelected(item)
+//        }
+//    }
 
 
 }
